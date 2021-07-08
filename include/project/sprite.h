f8d149154bdf04503adb7fdee822ad9603388229
Suven Pandey <pandeysuven@gmail.com>
2021-07-08 10:18:06 +0545

    Add position to Sprite
    Co-authored-by: Suprim Shrestha <suprim.shresth@gmail.com>

--- include/project/sprite.h
+++ include/project/sprite.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <utility>
 #include <project/common.h>
 #include <project/texture.h>
 #include <glm/glm.hpp>
@@ -13,10 +14,15 @@
 class Sprite
 {
 protected:
+    std::pair<float, float> position;
     std::string texture;
 
 public:
+    Sprite();
+    void setPosition(std::pair<float, float> newPosition);
+    std::pair<float, float> getPosition() const;
     void setTexture(std::string newTexture);
     std::string getTexture() const;
     virtual void draw(std::string shader) = 0;
+    virtual ~Sprite();
 };
--- src/sprite.cpp
+++ src/sprite.cpp
@@ -1,4 +1,10 @@
 #include <project/sprite.h>
 
+Sprite::Sprite()
+{
+    position = std::make_pair(-1.0f, -1.0f);
+}
+void Sprite::setPosition(std::pair<float, float> newPosition) { position = newPosition; }
+std::pair<float, float> Sprite::getPosition() const { return position; }
 void Sprite::setTexture(std::string newTexture) { texture = newTexture; }
 std::string Sprite::getTexture() const { return texture; }
